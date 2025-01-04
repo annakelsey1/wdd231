509a07319cf57a83c71d785df6d96d4bb5aec783
annakelsey1 <anna.kelsey@gmail.com>
2025-01-04 16:37:55 -0700

    most html done

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
-document.getElementById("current-year").textContent = new Date().getFullYear();
-document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("current-year").textContent = new Date().getFullYear();
+    document.getElementById("lastModified").textContent = "Last updated: " + new Date(document.lastModified).toLocaleString();
+  });
+  
 
