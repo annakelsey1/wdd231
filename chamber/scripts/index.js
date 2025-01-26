5ea78f0e6d82534331b68db7226346b81838c55d
annakelsey1 <anna.kelsey@gmail.com>
2025-01-26 16:25:13 -0700

    making it's own css and js files

---
+++ chamber/scripts/index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("current-year").textContent = new Date().getFullYear();
+    document.getElementById("lastModified").textContent = `Last updated: ${new Date(document.lastModified).toLocaleString()}`;
+});
+
+const hamburgerElement = document.querySelector('#myButton');
+const navElement = document.querySelector('#animateMe');
+
+hamburgerElement.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+});
+
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
