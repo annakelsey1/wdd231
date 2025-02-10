a57753417a462b75a47d62c6103c2071089892b0
annakelsey1 <anna.kelsey@gmail.com>
2025-02-09 22:38:27 -0700

    added nav elements and styling, will need to redo colors and where they are assinged.

--- finalproject/scripts/index.js
+++ finalproject/scripts/index.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("current-year").textContent = new Date().getFullYear();
     document.getElementById("lastModified").textContent = `Last updated: ${new Date(document.lastModified).toLocaleString()}`;
+
+    const hamburgerElement = document.querySelector('#myButton');
+    const navElement = document.querySelector('#animateMe');
+    hamburgerElement.addEventListener('click', () => {
+        navElement.classList.toggle('open');
+        hamburgerElement.classList.toggle('open');
+        hamburgerElement.setAttribute('aria-label', hamburgerElement.classList.contains('open') ? "Close menu" : "Open menu");
+    });
 });
